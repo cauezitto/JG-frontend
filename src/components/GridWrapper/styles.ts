a93170d395a6695531b7963f485b970e44322a56
cauezitto <caue.matheus.granrodeo@gmail>
2021-02-28 18:20:22 -0300

    Retirando flash ao carregar componente do produto

--- src/components/GridWrapper/styles.ts
+++ src/components/GridWrapper/styles.ts
@@ -13,6 +13,7 @@ export const Wrapper = styled.div`
 
     .center {
       display: flex;
+      flex-direction: column;
       width: 100%;
       align-items: center;
       justify-content: center;
